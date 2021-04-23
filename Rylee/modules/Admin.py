530cdb54198dc8e68d540f01cf2b81187391297c
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-23 17:31:30 +0530

    Create Admin.py

---
+++ Rylee/modules/Admin.py
@@ -0,0 +1,13 @@
+from Rylee import thot, CMD_HELP
+import os
+from Rylee.events import is_admin
+from Rylee.Function import can_promote_users, get_user
+from telethon import events
+
+
+@tbot.on(events.NewMessage(pattern="^[!/?]promote ?(.*)"))
+@is_admin
+async def _(event, perm):
+ if not perm.add_admins:
+      return await event.reply("You are missing the following rights to use this command:CanAddAdmins!")
+ await event.reply("hi")
