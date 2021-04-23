18a3232efd3180bde6323ba4d7d4807311541d0d
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-23 15:06:02 +0530

    Create Function.py

---
+++ Rylee/Function.py
@@ -0,0 +1,28 @@
+from Rylee import tbot
+
+async def get_user(event):
+    """ Get the user from argument or replied message. """
+    args = event.pattern_match.group(1).split(" ", 1)
+    extra = None
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user_obj = await event.client.get_entity(previous_message.sender_id)
+        extra = event.pattern_match.group(1)
+    elif args:
+        user = args[0]
+        if len(args) == 2:
+            extra = args[1]
+
+        if user.isnumeric():
+            user = int(user)
+
+        if not user:
+            await event.reply("I don't know who you're talking about, you're going to need to specify a user...!")
+            return
+        try:
+            user_obj = await tbot.get_entity(user)
+        except (TypeError, ValueError) as err:
+            await event.reply(str(err))
+            return None
+
+    return user_obj, extra
