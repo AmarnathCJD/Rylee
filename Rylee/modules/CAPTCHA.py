f50f06451d81291dd0b6893ddca41557d4136b67
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-24 12:40:53 +0530

    Update CAPTCHA.py

--- Rylee/modules/CAPTCHA.py
+++ Rylee/modules/CAPTCHA.py
@@ -10,21 +10,68 @@ turnoff = ["off", "no", "disable"]
 @tbot.on(events.NewMessage(pattern="^[!?/]captcha ?(.*)"))
 @is_admin
 async def lel(event, perm):
- if not perm.change_info:
-         await event.reply("You are missing the following rights to use this command:CanChangeInfo!")
-         return
+ if "captchamode" or "captchakick" or "captchakicktime" in event.text:
+   return
  args = event.pattern_match.group(1)
  settings = sql.get_mode(event.chat_id)
  if not args:
-  if settings == False:
-   await event.reply("Currently Welcome CAPTCHAs are disabled for this Chat.")
-  elif settings == True:
-   await event.reply("Currently Welcome CAPTCHAs are enabled for this Chat.")
+  if settings == True:
+   await event.reply("Users will be asked to complete a CAPTCHA before being allowed to speak in the chat.\nTo change this setting, try this command again followed by one of yes/no/on/off")
+  elif settings == False:
+   await event.reply("Users will NOT be muted when joining the chat.\nTo change this setting, try this command again followed by one of yes/no/on/off")
  elif args in turnon:
+  if not perm.change_info:
+         await event.reply("You are missing the following rights to use this command:CanChangeInfo!")
+         return
   mode = True
-  await event.reply("Enabled Welcome CAPTCHAs for this chat.")
+  await event.reply("CAPTCHAs have been enabled. I will now mute people when they join.")
   x = sql.set_mode(event.chat_id, mode)
  elif args in turnoff:
+  if not perm.change_info:
+         await event.reply("You are missing the following rights to use this command:CanChangeInfo!")
+         return
   mode = False
-  await event.reply("Disabled Welcome CAPTCHAs for this chat.")
+  await event.reply("CAPTCHAs have been disabled. Users can join normally.")
   x = sql.set_mode(event.chat_id, mode)
+
+@tbot.on(events.NewMessage(pattern="^[!?/]captchamode ?(.*)"))
+@is_admin
+async def lel(event, perm):
+ options = ["math", "button", "text", "multibutton"]
+ if not perm.change_info:
+         await event.reply("You are missing the following rights to use this command:CanChangeInfo!")
+         return
+ settings = sql.get_style(event.chat_id)
+ if not args:
+  if settings == False:
+   await event.reply("Enable CAPTCHAs First.!")
+  elif settings in options:
+   if settings == "button":
+    text = """
+The current CAPTCHA mode is: button
+Button CAPTCHAs simply require a user to press a button in their welcome message to confirm they're human.
+
+Available CAPTCHA modes are: button/math/text/multibutton
+"""
+   elif settings == "text":
+    text = """
+The current CAPTCHA mode is: text
+Text CAPTCHAs require the user to answer a CAPTCHA containing letters and numbers.
+
+Available CAPTCHA modes are: button/math/text/mutlibutton
+"""
+   elif settings == "math":
+    text = """
+The current CAPTCHA mode is: math
+Math CAPTCHAs require the user to solve a basic maths question. Please note that this may discriminate against users with little maths knowledge.
+
+Available CAPTCHA modes are: button/math/text/multibutton
+"""
+   elif settings == "multibutton":
+    text = """
+The current CAPTCHA mode is: multibutton
+Multibutton CAPTCHAs require users to solve a button puzzle
+
+Available CAPTCHA modes are: button/math/text/multibutton
+"""
+  await event.reply(text)
