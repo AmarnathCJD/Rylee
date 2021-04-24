6c7b265fc71e1d2b74f5d19fc6fff12c768763b7
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-24 11:27:51 +0530

    Update captcha_sql.py

--- Rylee/modules/sql/captcha_sql.py
+++ Rylee/modules/sql/captcha_sql.py
@@ -5,7 +5,7 @@ from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 class Captcha(BASE):
     __tablename__ = "cla"
 
-    chat_id = Column(Integer, primary_key=True)
+    chat_id = Column(String(14), primary_key=True)
     mode = Column(Boolean)
     time = Column(Integer)
     style = Column(UnicodeText)
